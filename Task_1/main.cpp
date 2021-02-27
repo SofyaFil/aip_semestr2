59bd9a8a5c8a5e8a2d728205cb0e8e813f88d99f
SofyaFil <79421158+SofyaFil@users.noreply.github.com>
2021-02-27 13:51:30 +0300

    Task_1

---
+++ Task_1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+//Задача 1
+//Вывести на экран Первую букву имени из символов "*" (звездочка)
+
+int main()
+{
+    cout << "            *****     " << endl;
+    cout << "         ****   ***   " << endl;
+    cout << "       ****        ** " << endl;
+    cout << "     ****             " << endl;
+    cout << "   ****               " << endl;
+    cout << "   ****               " << endl;
+    cout << "   ****               " << endl;
+    cout << "   ****               " << endl;
+    cout << "   ****               " << endl;
+    cout << "     ****             " << endl;
+    cout << "      ****         ** " << endl;
+    cout << "        ****     ***  " << endl;
+    cout << "            *****     " << endl;
+
+
+    return 0;
+}
+
+
+
+/* |x| + x*x*x*x*x, если x=−2 Ответ: -30 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
